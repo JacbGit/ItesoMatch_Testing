587098be6ab43f9a33866d16759481f4888cd62c
Luis <67877206+xiluisx@users.noreply.github.com>
2024-04-23 19:40:10 -0600

    Added swipe and fixed routes

---
+++ views/swipe.js
@@ -0,0 +1,78 @@
+function onMouseHoverLeft() {
+    card.classList.add('card__animate--left')
+}
+
+function onMouseOutLeft(e) {
+    card.classList.remove('card__animate--left')
+}
+
+function onMouseHoverRight() {
+    card.classList.add('card__animate--right')
+}
+
+function onMouseOutRight(e) {
+    card.classList.remove('card__animate--right')
+}
+
+function onClickLeft() {
+    swipeText.innerText = "No match!"
+    card.classList.add('fade__animation')
+    card.classList.add('fade__out')
+    setTimeout(() => {
+        swipeText.innerText = ""
+        card.classList.remove('fade__out')
+    },1000) 
+}
+
+function onClickRight() {
+    swipeText.innerText = "Match!"
+    card.classList.add('fade__animation')
+    card.classList.add('fade__out')
+    setTimeout(() => {
+        swipeText.innerText = ""
+        card.classList.remove('fade__out')
+    },1000) 
+}
+
+document.querySelector('#swipeContainer').innerHTML = String.raw`
+    <div>
+        <div class="w-full swipe__body h-auto">
+            <div class="card relative card__animate" id="card">
+                <div class="w-full h-full absolute z-index-10">
+                    <div class="flex justify-center w-100 h-full">
+                        <div class="w-50 h-full" id="cardLeftTrigger">
+                            
+                        </div>
+                        <div class="w-50 h-full" id="cardRightTrigger">
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="d-flex flex-column text-center">
+                    <p class="card__title">
+                        Andrea Rojo - 23
+                    </p>
+                    <div class="separator w-full my"></div>
+                    <div>
+                        <img src="../assets/karen.jpeg" style="height: 300px; width:300px">
+                    </div>
+                </div>
+            </div>
+
+            <p id="swipeText" class="text-white"></p>
+        </div>
+    </div>
+`
+const card = document.getElementById('card')
+
+const cardLeftTrigger = document.getElementById('cardLeftTrigger')
+cardLeftTrigger.addEventListener('mouseover', onMouseHoverLeft)
+cardLeftTrigger.addEventListener('mouseout', onMouseOutLeft)
+cardLeftTrigger.addEventListener('click', onClickLeft)
+
+const cardRightTrigger = document.getElementById('cardRightTrigger')
+cardRightTrigger.addEventListener('mouseover', onMouseHoverRight)
+cardRightTrigger.addEventListener('mouseout', onMouseOutRight)
+cardRightTrigger.addEventListener('click', onClickRight)
+
+const swipeText = document.getElementById('swipeText')
