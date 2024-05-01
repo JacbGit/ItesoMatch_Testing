d48b48d8ac1a4cbeae5c042d0d18b84a31ce1729
Luis <67877206+xiluisx@users.noreply.github.com>
2024-05-01 17:39:39 -0600

    Moved app to single file for async loading

---
+++ server/app.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const cors = require('cors')
+const { Server } = require('socket.io')
+const router = require('./router')
+const cookieParser = require('cookie-parser')
+const morgan = require('morgan')
+
+const app = express()
+const server = require('http').createServer(app)
+const io = new Server(server, {
+    cors: {
+        origin: "*"
+    }
+})
+
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors())
+
+app.use("/api", router)
+
+app.use('*', (req, res) => {
+    res.status(404).send("Not found")
+})
+
+io.on('connection', (socket) => {
+    console.log("New socket connection")
+
+    socket.on("message", (data) => {
+        console.log("New message:", data)
+        io.sockets.emit("new-message", data)
+    })
+})
+
+module.exports = server
--- server/index.js
+++ server/index.js
@@ -1,30 +1,15 @@
-const express = require('express')
-const cors = require('cors')
-const { Server } = require('socket.io')
+const mongoose = require("mongoose");
+const server = require("./app")
 
-const app = express()
-const server = require('http').createServer(app)
-const io = new Server(server, {
-    cors: {
-        origin: "*"
-    }
-})
+const mongoDB = "mongodb://127.0.0.1/andrea";
 
-app.use(cors())
+main().catch((err) => console.log(err));
 
-app.get('*', (req, res) => {
-    res.send("Server response!")
-})
-
-io.on('connection', (socket) => {
-    console.log("New socket connection")
-
-    socket.on("message", (data) => {
-        console.log("New message:", data)
-        io.sockets.emit("new-message", data)
+async function main() {
+    await mongoose.connect(mongoDB);
+    server.listen(3000, () => {
+        console.log("Server listening on http://localhost:3000")
     })
-})
+}
 
-server.listen(3000, () => {
-    console.log("Server listening on http://localhost:3000")
-})
+main()
