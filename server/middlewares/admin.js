73d74cd276b78766003a72702e45188070ea69b7
Luis <67877206+xiluisx@users.noreply.github.com>
2024-05-01 17:40:35 -0600

    Added temp admin middleware

---
+++ server/middlewares/admin.js
@@ -0,0 +1,10 @@
+module.exports = (req, res, next) => {
+    const adminHeader = req.get('adminToken')
+    if(adminHeader === 'admin123')
+    {
+        return next()
+    }
+    res.status(401).json({ok:false, err: {
+        message: "No admin token!"
+    }})
+}
