912758d25ae8c99cd2a5f9b92d7122ef5455f1dc
Luis <67877206+xiluisx@users.noreply.github.com>
2024-05-01 19:04:40 -0600

    Revert "Revert "Merge branch 'master' of https://github.com/xiluisx/ProyectoWeb""
    This reverts commit e0122f7ae0c814d291e2f1cece9dd91078fc76b5.

--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose')
+const { DB_URI, PORT } = require('./utils/config')
 const server = require('./app')
 
-const mongoDB = 'mongodb://127.0.0.1/andrea'
+const mongoDB = DB_URI
 
 main().catch((err) => console.log(err))
 
 async function main () {
   await mongoose.connect(mongoDB)
-  server.listen(3000, () => {
+  server.listen(PORT, () => {
     console.log('Server listening on http://localhost:3000')
   })
 }
---
+++ server/utils/config.js
@@ -0,0 +1,9 @@
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+exports.PORT = process.env.PORT || 3000
+
+exports.JWT_SECRET = process.env.JWT_SECRET || 'andrea'
+
+exports.DB_URI = process.env.NODE_ENV === 'dev' ? process.env.DB_URI_DEV : process.env.DB_URI
