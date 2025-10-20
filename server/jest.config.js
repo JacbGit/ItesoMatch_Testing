71bcd3de0aa959ed08fe49ec02c94e5dac85713b
Alan Solorio <69700672+AlanElPlatano@users.noreply.github.com>
2025-10-20 11:13:07 -0600

    fix: add tests for backend login endpoint
    a y u d a !!!

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,14 +1,14 @@
 module.exports = {
-  testEnvironment: 'node',
-  coveragePathIgnorePatterns: ['/node_modules/'],
-  testMatch: ['**/__tests__/**/*.test.js'],
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: "node",
+  coveragePathIgnorePatterns: ["/node_modules/"],
+  testMatch: ["**/__tests__/**/*.test.js"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   collectCoverageFrom: [
-    'modules/**/*.js',
-    'middlewares/**/*.js',
-    'utils/**/*.js',
-    '!**/node_modules/**',
+    "modules/**/*.js",
+    "middlewares/**/*.js",
+    "utils/**/*.js",
+    "!**/node_modules/**",
   ],
   verbose: true,
   testTimeout: 15000,
-}
+};
--- server/jest.setup.js
+++ server/jest.setup.js
@@ -1,14 +1,14 @@
-const mongoose = require('mongoose')
-const { DB_URI } = require('./utils/config')
+const mongoose = require("mongoose");
+const { DB_URI } = require("./utils/config");
 
 beforeAll(async () => {
   if (mongoose.connection.readyState === 0) {
-    await mongoose.connect(DB_URI)
-    console.log('Connected to MongoDB for testing')
+    await mongoose.connect(DB_URI);
+    console.log("Connected to MongoDB for testing");
   }
-})
+});
 
 afterAll(async () => {
-  await mongoose.connection.close()
-  console.log('Closed MongoDB connection')
-})
+  await mongoose.connection.close();
+  console.log("Closed MongoDB connection");
+});
